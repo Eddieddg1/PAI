69db195c99258a63c16110b2252fdfc628a7e257
Eddieddg1 <55411637+Eddieddg1@users.noreply.github.com>
2024-07-15 17:18:14 +0200

    Update l.sh

--- l.sh
+++ l.sh
@@ -48,7 +48,7 @@ mount /dev/${DISK}${PART_SUFFIX}3 /mnt || exit 1
 
 read -p "Packages?: " PAC || exit 1
 
-pacstrap -K /mnt base base-devel linux linux-firmware fastfetch htop nano networkmanager $PAC || exit 1
+pacstrap -K /mnt base base-devel linux linux-firmware fastfetch htop nano plasma sddm networkmanager xorg-server xorg-apps xorg-xinit nvidia-dkms $PAC || exit 1
 
 genfstab -U /mnt >> /mnt/etc/fstab || exit 1
 
