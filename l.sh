dd833e0bd54c857367bc3a0487024b74ac29867a
Eddieddg1 <55411637+Eddieddg1@users.noreply.github.com>
2024-07-16 22:41:02 +0200

    Update l.sh
    Updated the driver thing, since I had some trouble with that when testing.

--- l.sh
+++ l.sh
@@ -59,27 +59,39 @@ mount /dev/${DISK}${PART_SUFFIX}1 /mnt/boot || exit 1
 swapon /dev/${DISK}${PART_SUFFIX}2 || exit 1
 mount /dev/${DISK}${PART_SUFFIX}3 /mnt || exit 1
 
-read -p "Packages?: " PAC || exit 1
+echo "base base-devel linux linux-firmware fastfetch htop nano plasma sddm networkmanager iwd xorg-server xorg-apps xorg-xinit"
+read -p "Do you want to add packages? (drivers get added after this): " PAC || exit 1
 
 pacstrap -K /mnt base base-devel linux linux-firmware fastfetch htop nano plasma sddm networkmanager iwd xorg-server xorg-apps xorg-xinit $PAC || exit 1
 
-graphics_card=$(lspci -v | grep -A1 -e VGA -e 3D | grep "VGA compatible controller")
+read -p "Do you want to use open source drivers? y/n: " OPENSOURCE
 
-if [ -z "$graphics_card" ]; then
-    echo "No VGA compatible controller found."
-    exit 1
+if [[ $OPENSOURCE == y ]]; then
+    echo "Available open source driver packages:"
+    pacman -Ss xf86-video | grep "/xf86-video-" | awk '{print NR".", $1}'
+
+    driver_count=$(pacman -Ss xf86-video | grep "/xf86-video-" | wc -l)
+
+    if [ $driver_count -eq 0 ]; then
+        echo "No open source driver packages found."
+        exit 1
+    fi
+
+    read -p "Choose the driver package you want to install (enter number): " DRIVER_CHOICE
+
+    if ! [[ "$DRIVER_CHOICE" =~ ^[1-$driver_count]$ ]]; then
+        echo "Invalid selection. Please enter a number between 1 and $driver_count."
+        exit 1
+    fi
+
+    selected_driver=$(pacman -Ss xf86-video | grep "/xf86-video-" | awk 'NR=='$DRIVER_CHOICE'{print $1}')
+
+    pacstrap -K /mnt $selected_driver || exit 1
+else
+    read -p "You need to specify what drivers you want: " DRIVERS
+    pacstrap -K /mnt $DRIVERS || exit 1
 fi
 
-graphics_model=$(echo "$graphics_card" | sed -n 's/^.*: \(.*\)$/\1/p')
-
-driver_packages=$(pacman -Ss xf86-video | grep "/xf86-video-" | awk '{print $1}')
-
-if [ -z "$driver_packages" ]; then
-    echo "No xf86-video driver packages found."
-    exit 1
-fi
-
-pacstrap -K /mnt $(echo "$driver_packages" | head -n 1) || exit 1
 
 genfstab -U /mnt >> /mnt/etc/fstab || exit 1
 
